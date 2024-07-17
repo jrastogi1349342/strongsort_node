86f4da64058f80035f7cf6bd205b16569b8b1765
Jai Rastogi <jai1rastogi@gmail.com>
2024-07-17 16:17:18 -0400

    Debugging multi agent launch files, clustering day 1 cont (changed launch file, cmake)

--- launch/strongsort_node_launch.py
+++ launch/strongsort_node_launch.py
@@ -1,23 +1,59 @@
 import launch
-import launch_ros.actions
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 # To run: 
 # Cmd window 1: colcon build
 # Cmd window 2: ros2 launch strongsort_node strongsort_node_launch.py 
 #    --> for reference: (ros2 launch {package_name} {name of launch file})
-def generate_launch_description(): 
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
+def launch_setup(context, *args, **kwargs): 
+    strongsort_node = Node(
             package='strongsort_node', 
-            executable='track_ros_setup.py', # connects to entry point in setup.py
+            # connects to entry point in setup.py
+            executable='track_ros_setup.py', 
             name='strongsort', 
-            output='screen',
-            emulate_tty=True, 
+            # output='screen',
+            # emulate_tty=True, 
             parameters=[
-                {'video_topic': '/rm_vlc_leftfront/image', 
-                 'name_space': 'A'}
-            ]
+                {
+                    'video_topic': '/rm_vlc_leftfront/image', 
+                    'name_space': LaunchConfiguration('name_space'), 
+                    "robot_id": LaunchConfiguration('robot_id'), 
+                    "max_nb_robots": LaunchConfiguration('max_nb_robots')
+                 }
+            ], 
+            # namespace=LaunchConfiguration('namespace')
         )
+    
+    association_node = Node(
+        package='strongsort_node', 
+        executable='associations_ros_driver.py', 
+        name='associations', 
+        output='screen', 
+        emulate_tty=True,
+        parameters=[
+            {
+                'name_space': LaunchConfiguration('name_space'), 
+                "robot_id": LaunchConfiguration('robot_id'), 
+                "max_nb_robots": LaunchConfiguration('max_nb_robots'), 
+            }
+        ]
+    )
+    
+    return [
+        # DeclareLaunchArgument('prefix', default_value=LaunchConfiguration('namespace').perform(context).replace("/",'')),
+        strongsort_node, 
+        association_node
+    ]
+
+
+def generate_launch_description():     
+    return launch.LaunchDescription([
+        DeclareLaunchArgument('name_space', default_value='A', description=''), 
+        DeclareLaunchArgument('robot_id', default_value='0', description=''), 
+        DeclareLaunchArgument('max_nb_robots', default_value='2', description=''), 
+        OpaqueFunction(function=launch_setup)
     ])
     
